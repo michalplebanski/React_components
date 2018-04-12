4869abb7e48d88964c7c52aea2308ed5acad49d6
Michal <michalplebanski@gmail.com>
2018-04-12 17:11:25 +0200

    Adding props into MovieList

--- components.js
+++ components.js
@@ -1,29 +1,4 @@
-var movies = [
-	{
-		id: 1,
-		title: 'Harry Potter',
-		desc: 'Opowiesc o czarodzieju',
-		src: 'image/harry.jpg'
-	},
-	{
-		id: 2,
-		title: 'Król Lew',
-		desc: 'Film o królu sawammy',
-		src: 'image/król.jpg'
-	},
-	{
-		id: 3,
-		title: 'Piraci z Karaibów',
-		desc: 'Film o piratach',
-		src: 'image/piraci.jpg'
-	},
-	{
-		id: 4,
-		title: 'Władca pierścieni',
-		desc: 'Film przygodowy',
-		src: 'image/władca.jpg'
-	}
-];
+
 
 var Movie = React.createClass({
 	propTypes: {
@@ -71,19 +46,48 @@ var MovieSrc = React.createClass({
 	}
 });
 
-var moviesElements = movies.map(function(movie) {
-	return React.createElement(Movie, {key:movie.id, movie: movie})
-});
-
 var MovieList = React.createClass({
+	propTypes: {
+		movies: React.PropTypes.array.isRequired,
+	},
 	render: function() {
+		var moviesElements = this.props.movies.map(function(movie) {
+			return React.createElement(Movie, {key:movie.id, movie: movie})
+		});
 		return React.createElement('ul', {}, moviesElements)
 	}
 });
 
 var element = React.createElement('div', {},
 	React.createElement('h1', {}, 'Lista'),
-	React.createElement(MovieList, {})
+	React.createElement(MovieList, {
+		 movies: [
+			{
+				id: 1,
+				title: 'Harry Potter',
+				desc: 'Opowiesc o czarodzieju',
+				src: 'image/harry.jpg'
+			},		
+			{
+				id: 2,
+				title: 'Król Lew',
+				desc: 'Film o królu sawammy',
+				src: 'image/król.jpg'
+			},
+			{
+				id: 3,
+				title: 'Piraci z Karaibów',
+				desc: 'Film o piratach',
+				src: 'image/piraci.jpg'
+			},
+			{
+				id: 4,
+				title: 'Władca pierścieni',
+				desc: 'Film przygodowy',
+				src: 'image/władca.jpg'
+			}
+		]
+	})
 );
 
 ReactDOM.render(element, document.getElementById('app'));
